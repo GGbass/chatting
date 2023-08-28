97f89ddbda883f4a9ef4b3969da28c6475e0aa18
Gonzalo Garcia Bastidas <ggonzalo.bastidas@gmail.com>
2023-08-28 14:25:38 +0200

    connecting chat

--- chatting/settings.py
+++ chatting/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'contact',
     'task',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -119,10 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+ #   '/var/www/static/',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
